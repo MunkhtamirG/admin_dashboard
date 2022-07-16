5807f97a7708c8de1be48dceba891ef29cd41ac3
Ozy <munkhtamir.g@outlook.com>
2022-07-17 01:41:01 +0800

    MOD

--- server/server.js
+++ server/server.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
 
-app.get("/", (req, res) => {
-  res.send("hi");
+app.get("/api/users", (req, res) => {
+  fs.readFile("./data/users.json", "utf-8", (err, users) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.send({ status: "Success", data: JSON.parse(users) });
+    }
+  });
 });
+
+app.get("/api/foods", (req, res) => {
+  fs.readFile("./data/foods.json", "utf-8", (err, foods) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.send({ status: "Success", data: JSON.parse(foods) });
+    }
+  });
+});
+
 app.listen(3000, () => {
   console.log("Running 3000");
 });

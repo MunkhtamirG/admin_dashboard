d1fd4b0c6df2036d6c10e7d20ae6b16d706c6e7d
Ozy <munkhtamir.g@outlook.com>
2022-07-18 18:24:57 +0800

    Changes to be committed: 	deleted:    client/.env deleted:    client/.eslintrc.js 	modified:   client/.gitignore 	modified:   client/README.md 	deleted:    client/craco.config.js 	modified:   client/package-lock.json 	modified:   client/package.json 	modified:   client/public/index.html 	deleted:    client/src/App.css 	modified:   client/src/App.js 	deleted:    client/src/components/Button.jsx 	deleted:    client/src/components/Cart.jsx 	deleted:    client/src/components/Charts/LineChart.jsx 	deleted:    client/src/components/Charts/Pie.jsx 	deleted:    client/src/components/Charts/SparkLine.jsx 	deleted:    client/src/components/Charts/Stacked.jsx

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import './index.css';
-import App from './App';
-import { ContextProvider } from './contexts/ContextProvider';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { UserProvider } from "./contexts/UserContext";
+import { OrderProvider } from "./contexts/OrderContext";
 ReactDOM.render(
   <React.StrictMode>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
+    <UserProvider>
+      <OrderProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </OrderProvider>
+    </UserProvider>
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
